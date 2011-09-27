8db8399b9e83434d72cfd65d1f581979e16240f4
Will M. Farr <w-farr@northwestern.edu>
2011-09-27 16:55:09 -0500

    Moved to clojure 1.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject nbody "1.0.0-SNAPSHOT"
   :description "Nbody algorithms"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :warn-on-reflection true)
